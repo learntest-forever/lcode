8d29411b7fbbbbb77d6f8c5a6a2685642f3e29d6
dongzhiqiang11 <dongzhiqiang3@jd.com>
2020-09-09 21:08:02 +0800

    reverse

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import(
+import (
+	"fmt"
+	"lcode/reverse"
 	"lcode/twosum"
 )
 
-func main(){
-	l1 := []int{1,3,2}
-	l2 := []int{2,4,6}
-	twosum.Twosum(&l1,&l2)
+func main() {
+	l1 := []int{1, 3, 2}
+	l2 := []int{2, 4, 6}
+	twosum.Twosum(&l1, &l2)
+	l3 := []int{1, 2, 5}
+	l4 := reverse.Myreverse(l3)
+	fmt.Printf("the l4 is :%v\n", l4)
 }
---
+++ reverse/myreverse.go
@@ -0,0 +1,9 @@
+package reverse
+
+import (
+	"fmt"
+)
+
+func Dreverse(s1 []int) []int{
+	return s1[-1:]
+}
