c2c54549d8ff47157b9742aa27fdefa9cffa2e73
dongzhiqiang11 <dongzhiqiang3@jd.com>
2020-09-10 10:00:44 +0800

    rename reverse to reverse2

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import(
+import (
+	"fmt"
+	"lcode/reverse2"
 	"lcode/twosum"
 )
 
-func main(){
-	l1 := []int{1,3,2}
-	l2 := []int{2,4,6}
-	twosum.Twosum(&l1,&l2)
+func main() {
+	l1 := []int{1, 3, 2}
+	l2 := []int{2, 4, 6}
+	twosum.Twosum(&l1, &l2)
+	l3 := []int{1, 2, 5, 7}
+	l5 := reverse2.Myreverse(l3, []int{})
+	fmt.Printf("the l5 is :%v\n", l5[:])
 }
---
+++ reverse2/myreverse.go
@@ -0,0 +1,9 @@
+package reverse2
+
+func Myreverse(s1 []int, s2 []int) []int {
+	s2 = append(s2, s1[len(s1)-1])
+	if len(s1) == 1 {
+		return s2
+	}
+	return Myreverse(s1[:len(s1)-1], s2)
+}
